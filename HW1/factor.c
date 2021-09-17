ea0e5304f32f42f36ab2255d68408c9c740865d0
aum05 <aumpathak@yahoo.com>
2021-09-17 12:41:08 +0530

    factor complete

--- HW1/factor.c
+++ HW1/factor.c
@@ -14,14 +14,14 @@ void factorization (int n) {
     // when the program gets to this loop,
     // n has become, or already was, odd.
     // Since, dividing it by 1 has no point, we start the loop from i = 3.
-    for (int i = 3; i < sqrt(n); i = i+2)
+    for (int i = 3; i < n/2; i = i+2)
     {
         while (n % i == 0)
         {
             printf("%d ", i);
             n /= i;
         }
-        
+
     }
 
     // this last condition handles the case where n is prime number greater than 2
@@ -30,8 +30,8 @@ void factorization (int n) {
     }
 }
 
-int main(int argc, char* argv[argc + 1]){
-    int n = argv[1];
+int main(int argc, char** argv){
+    int n = atoi(argv[1]);
     factorization(n);
     return EXIT_SUCCESS;
 }

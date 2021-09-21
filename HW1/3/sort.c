6e914193551e2ff4e2252df7209870f74428e601
aum05 <aumpathak@yahoo.com>
2021-09-22 01:47:42 +0530

    commit

--- HW1/3/sort.c
+++ HW1/3/sort.c
@@ -22,36 +22,64 @@ void intsort(int* arr[], int n){
     qsort(arr, n, sizeof(int), intcomp);
 }
 
-// Function to dynamically allocate memory for a string read from stdin
-char *getstring(){
-    char *str = NULL, *temp = NULL;
-    size_t size = 0, index = 0;
-    int c = EOF;
 
-    while (c) {
-        c = getc(stdin);
+void DeleteStrings(char ***strings, int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        free((*strings)[i]);
+    free(*strings);
+    *strings = NULL;
+}
 
-        // Check whether we have reached End-of-File or New-line
-        if (c == EOF || c == '\n')
-            c = 0;
+/* Print error message and exit. */
+void die(const char *msg) {
+    fprintf(stderr, "Error: %s\n", msg);
+    exit(EXIT_FAILURE);
+}
 
-        // If the size of the string is less than the current index
-        // We need to re-allocate memory
-        if (size <= index) {
-            size += 1;
-            temp = realloc(str, size);
-            if (!temp) {
-                free(str);
-                str = NULL;
-                break;
-            }
-            str = temp;
+char **StoreStrings(int *size) {
+    int allocated = INITIAL_STRING_ARRAY_SIZE;
+    char **strings, **temp;
+
+    *size = 0;
+    strings = malloc(allocated * sizeof(char*));
+    if (strings == NULL)
+        die("StoreStrings 1");
+
+    printf("Enter strings. Enter an empty string to quit.\n");
+
+    while (1) {
+        char *str = GetString();
+        if (str == NULL) {
+            DeleteStrings(&strings, *size);
+            die("StoreStrings 2");
         }
 
-        // create the whole string by adding each character at its index
-        str[index++] = c;
+        if (str[0] == '\0') {
+            free(str);
+            break;
+        }
+
+        if (*size == allocated) {
+            temp = realloc(strings, (allocated *= 2) * sizeof(char*));
+            if (temp == NULL) {
+                DeleteStrings(&strings, *size);
+                die("StoreStrings 3");
+            }
+            strings = temp;
+        }
+
+        strings[(*size)++] = str;
     }
-    return str;
+
+    /* Fit to size. */
+    temp = realloc(strings, *size * sizeof(char*));
+    if (temp == NULL) {
+        DeleteStrings(&strings, *size);
+        die("StoreStrings 4");
+    }
+
+    return temp;
 }
 
 int main(int argc, char** argv){

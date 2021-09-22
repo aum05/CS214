681350c044e2eaaf97106bfa3e7c4ba7748ef091
aum05 <aumpathak@yahoo.com>
2021-09-22 15:04:42 +0530

    commit for sort

--- HW1/3/sort.c
+++ HW1/3/sort.c
@@ -4,7 +4,7 @@
 
 // Comparator function for comparing strings
 static int strcompare(const void* x, const void* y){
-    return strcmp(*(const char**)x, *(const char**)y);
+    return strcasecmp(*(const char**)x, *(const char**)y);
 }
 
 // Comparator function for comparing integers
@@ -18,73 +18,68 @@ void strsort(const char* arr[], int n){
 }
 
 // Sort function for integers
-void intsort(int* arr[], int n){
+void intsort(int arr[], int n){
     qsort(arr, n, sizeof(int), intcomp);
 }
 
+char *get_strings(void){
+    char *string = NULL;//or calloc(1,sizeof(char)) for return "" (test by caller: *string == '\0')
+    int ch;
+    size_t len = 0;
 
-void DeleteStrings(char ***strings, int size) {
-    int i;
-    for (i = 0; i < size; i++)
-        free((*strings)[i]);
-    free(*strings);
-    *strings = NULL;
-}
-
-/* Print error message and exit. */
-void die(const char *msg) {
-    fprintf(stderr, "Error: %s\n", msg);
-    exit(EXIT_FAILURE);
-}
-
-char **StoreStrings(int *size) {
-    int allocated = INITIAL_STRING_ARRAY_SIZE;
-    char **strings, **temp;
-
-    *size = 0;
-    strings = malloc(allocated * sizeof(char*));
-    if (strings == NULL)
-        die("StoreStrings 1");
-
-    printf("Enter strings. Enter an empty string to quit.\n");
-
-    while (1) {
-        char *str = GetString();
-        if (str == NULL) {
-            DeleteStrings(&strings, *size);
-            die("StoreStrings 2");
-        }
-
-        if (str[0] == '\0') {
-            free(str);
-            break;
-        }
-
-        if (*size == allocated) {
-            temp = realloc(strings, (allocated *= 2) * sizeof(char*));
-            if (temp == NULL) {
-                DeleteStrings(&strings, *size);
-                die("StoreStrings 3");
-            }
-            strings = temp;
-        }
-
-        strings[(*size)++] = str;
+    while(EOF != (ch=fgetc(stdin)) && ch != '\n' ) {
+        string = (char*)realloc(string,  len + 2);//To realloc to each character is inefficient
+        string[len++] = ch;
     }
-
-    /* Fit to size. */
-    temp = realloc(strings, *size * sizeof(char*));
-    if (temp == NULL) {
-        DeleteStrings(&strings, *size);
-        die("StoreStrings 4");
-    }
-
-    return temp;
+    if(string)
+        string[len] = '\0';
+    return string;//The value is NULL when there is no input substantial.
 }
 
 int main(int argc, char** argv){
-    char option[] = "-n";
-    
+    //char option[] = "-n";
+    int index = 0;
+    int count = 0;
+
+    if(argc == 2){
+        if(strcmp(argv[1], "-n") == 0){
+        int *numbers = NULL;
+        int number;
+        while (!feof(stdin))
+        {
+            scanf("%d\n", &number);
+            numbers = (int*) realloc (numbers, (index+1)*sizeof(*numbers));
+            numbers[index] = number;
+            ++index;
+        }
+
+        intsort(numbers, index);
+
+        printf("\nSorted:\n");
+
+        for(count = 0; count < index; ++count){
+            printf("%d\n", numbers[count]);
+        }
+        free(numbers);
+        }
+    }
+    else{
+        char **strings = NULL;
+        char *string;
+        for(index = 0; (string=get_strings()); ++index){
+            strings = (char**) realloc (strings, (index+1)*sizeof(*strings));
+            strings[index] = string;
+        }
+        strsort((const char**)strings, index);
+
+        printf("\nSorted:\n");
+
+        for(count = 0; count < index; ++count){
+            printf("%s\n", strings[count]);
+            free(strings[count]);
+        }
+        free(strings);
+    }
     
     return EXIT_SUCCESS;
 }
